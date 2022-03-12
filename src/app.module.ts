5ac5499a5dd892476a6ec24168be9b8f6169ec81
Александр Чазов <chazovby@gmail.com>
2022-03-12 12:45:04 +0300

    feat: connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  controllers: [AppController],  // регистрируем контроллер в модули
-  providers: [AppService]  // любой переиспользуемый компонент, всё что содержит логику и может использоваться в других компонентах
+  controllers: [],  // регистрируем контроллер в модули
+  providers: [],  // любой переиспользуемый компонент, всё что содержит логику и может использоваться в других компонентах
                            // чтобы сервис работал нам нужно сделать его инъекцию в контроллер
+  imports: [    // чтобы импортировать другие модули
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),        // порт по умолчанию для postgres
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB, // название БД
+      models: [],
+      autoLoadModels: true  // чтобы Sequelize создавал таблицы в БД на основании тех моделей, которые мы будем создавать
+    }),
+    UsersModule,
+  ]
 })
 
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()  // чтобы класс стал провайдером, т.е. этот сервис мы будем внедрять в контроллер, т.е. делать инъекцию
-export class AppService {
-  getUsers() {
-    return [{id: 1, name: 'Ulbi TV'}]
-  }
-}
