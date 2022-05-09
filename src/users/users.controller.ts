b6262397e7ef5ad08ed6e3f2c099a1e3cb69b682
Александр Чазов <chazovby@gmail.com>
2022-05-09 21:57:54 +0300

    feat: adding user's role and ban user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,8 @@ import { User } from './users.model';
 // import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { Roles } from '../auth/roles-auth.decorator';
 import { RolesGuard } from '../auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 
 @ApiTags('Users')
 @Controller('users')
@@ -13,14 +15,14 @@ export class UsersController {
 
   constructor(private userService: UsersService) {}
 
-  @ApiOperation({ summary: 'User creation' })
+  @ApiOperation({ summary: 'User creation' }) // описание
   @ApiResponse({ status: 200, type: User })  // описываем статус ответа и данные, которые он вернёт
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.userService.createUser(userDto)
   }
 
-  @ApiOperation({ summary: 'Get all users' })
+  @ApiOperation({ summary: 'Get all users' }) // описание
   @ApiResponse({ status: 200, type: [User] })  // описываем статус ответа и данные, которые он вернёт
   @Roles("ADMIN")    // пользователю с какими ролями будет доступен этот метод
   //@UseGuards(JwtAuthGuard)  // можем тут использовать JwtAuthGuard чтобы ограничить неавторизованным пользователям
@@ -29,5 +31,23 @@ export class UsersController {
   @Get()
   getAll() {
     return this.userService.getAllUsers()
+  }
+
+  @ApiOperation({ summary: 'Add role' }) // описание
+  @ApiResponse({ status: 200 })  // будет возвращать двухсотый статус код
+  @Roles("ADMIN")    // пользователю с какими ролями будет доступен этот метод
+  @UseGuards(RolesGuard)
+  @Post("/role") // добавляем префикс к эндпойнту
+  addRole(@Body() dto: AddRoleDto) {
+    return this.userService.addRole(dto);
+  }
+
+  @ApiOperation({ summary: 'Ban user' }) // описание
+  @ApiResponse({ status: 200 })  // будет возвращать двухсотый статус код
+  @Roles("ADMIN")    // пользователю с какими ролями будет доступен этот метод
+  @UseGuards(RolesGuard)
+  @Post("/ban") // добавляем префикс к эндпойнту
+  banUser(@Body() dto: BanUserDto) {
+    return this.userService.banUser(dto);
   }
 }
